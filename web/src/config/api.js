305e1c415097681e790a55bc2bbf39102ba8f36c
Marouane Dbibih <marrouane.dbibih@gmail.com>
2025-10-24 00:16:22 +0100

    Update for vps deploymeny

--- web/src/config/api.js
+++ web/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://158.69.254.33:5000';
 
 export default API_BASE_URL;
